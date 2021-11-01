ae271186c7a99fa5daa86debf9724b5825e99658
Alex C <47911421+AlexxCabrera@users.noreply.github.com>
2021-11-01 01:49:07 -0700

    Added subtraction using 2's compliment

--- thirtytwoBitAdder.v
+++ thirtytwoBitAdder.v
@@ -1,10 +1,15 @@
+//32-bit adder Adder/Subtractor 
 module thirtytwoBitAdder(
-input [31:0] A, [31:0] B, [0:0] Cin,
+input [31:0] A, [31:0] B, [0:0] Cin, Sub,
 output [31:0] Sum, [0:0] Cout);
 
 wire C0;
+wire [31:0] Bn;
 
-sixteenBitAdder SBA0(A[15:0], 	B[15:0], 	Cin, 	Sum[15:0], 	C0);sixteenBitAdder SBA1(A[31:16], 	B[31:16], 	C0, 	Sum[31:16], 	Cout);
+//If Sub=1 then B's bits are flipped
+thirtytwoBitXOR invert(B,Sub,Bn);
+
+sixteenBitAdder SBA0(A[15:0], 	Bn[15:0], (Cin||Sub), 	Sum[15:0], 	C0);
+sixteenBitAdder SBA1(A[31:16], 	Bn[31:16], C0, 		Sum[31:16],	Cout);
 
 endmodule
-
