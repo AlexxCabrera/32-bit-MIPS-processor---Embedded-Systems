06152d56a7798ae0654e9b04cb446a14d5ca7908
jebbyx <93552380+jebbyx@users.noreply.github.com>
2021-11-04 16:40:21 -0700

    Update Mux21Testbench.v

--- MUX/Mux21Testbench.v
+++ MUX/Mux21Testbench.v
@@ -1,19 +1,19 @@
 module Mux21Testbench;
 
-reg in0, in1, s0;
+reg in0, in1, sel;
 wire out;
 
-Mux21 test (. out(out), .in0(in0), .in1(in1), .s0(s0) );
+	Mux21 test (. out(out), .in0(in0), .in1(in1), .sel(sel) );
 
 initial begin
 	
-		   in0 = 1'b0;	in1 = 1'b1;	s0 = 1'b0;
-		#5 in0 = 1'b0;	in1 = 1'b1;	s0 = 1'b1;
+		   in0 = 1'b0;	in1 = 1'b1;	sel = 1'b0;
+		#5 in0 = 1'b0;	in1 = 1'b1;	sel = 1'b1;
 end
 
 initial begin
 
-	$monitor ("Time: %0d, in0 = %b, in1 = %b, s0 = %b, Output = %.2b", $time, in0, in1, s0, out);
+	$monitor ("Time: %0d, in0 = %b, in1 = %b, s0 = %b, Output = %.2b", $time, in0, in1, sel, out);
 end
 
 endmodule
