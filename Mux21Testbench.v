daa1685044a836ba1716cd28e50c2a0c27f2d6ed
jebbyx <93552380+jebbyx@users.noreply.github.com>
2021-11-04 11:41:38 -0700

    Add files via upload

---
+++ Mux21Testbench.v
@@ -0,0 +1,26 @@
+module Mux21Testbench;
+
+reg in0, in1, s0;
+wire out;
+
+Mux21 test (. out(out), .in0(in0), .in1(in1), .s0(s0) );
+
+initial begin
+	
+		   in0 = 1'b0;	in1 = 1'b0;	s0 = 1'b0;
+		#5 in0 = 1'b0;	in1 = 1'b1;	s0 = 1'b0;
+		#5 in0 = 1'b1;	in1 = 1'b0;	s0 = 1'b0;
+		#5 in0 = 1'b1;	in1 = 1'b1;	s0 = 1'b0;
+		#5 in0 = 1'b0;	in1 = 1'b0;	s0 = 1'b1;
+		#5 in0 = 1'b0;	in1 = 1'b1;	s0 = 1'b1;
+		#5 in0 = 1'b1;	in1 = 1'b0;	s0 = 1'b1;
+		#5 in0 = 1'b1;	in1 = 1'b1;	s0 = 1'b1;
+
+end
+
+initial begin
+
+	$monitor ("Time: %0d, in0 = %b, in1 = %b, s0 = %b, Output = %.2b", $time, in0, in1, s0, out);
+end
+
+endmodule
