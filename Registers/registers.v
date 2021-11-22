cc080b6b4e3fe6f47491f365e07bf73ef9c62be7
Alex C <47911421+AlexxCabrera@users.noreply.github.com>
2021-11-21 16:34:38 -0800

    register block
    Has the inputs and outputs and 32 registers with 32 bits of data. Not completed

---
+++ Registers/registers.v
@@ -0,0 +1,6 @@
+module RegDstMux(input clk, [4:0] read_register1, [4:0] read_register2, [4:0] writeReg, [0:0] RegWrite, [31:0] writeData, 
+output reg [31:0] readData1, readData2);
+
+reg [31:0] registers [31:0];
+
+endmodule
