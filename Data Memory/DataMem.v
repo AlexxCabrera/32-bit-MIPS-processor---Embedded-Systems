e0274daf5264dfb0fa86e3f89b1dd3bd0a71ebba
Alex C <47911421+AlexxCabrera@users.noreply.github.com>
2021-11-22 16:15:07 -0800

    Updated
    updated sizes and added the needed slots of memory

--- Data Memory/DataMem.v
+++ Data Memory/DataMem.v
@@ -1,7 +1,7 @@
 /*INCOMPLETE*/
-//Dont know size for address, write data, or Read data
-module DataMem(input MemWrite, MemRead, Address, Write_data
-output Read_data );
+module DataMem(input MemWrite, MemRead,[31:0] Address, [31:0] Write_data
+output [31:0] Read_data );
 
+reg [31:0] memory [255:0]; // 2^32 slots are available in memory but for this project we are using 256 slots
 
 endmodule
