6e2f6e5011fc1a9e94c46b3c261e2f8a515744a2
Alex C <47911421+AlexxCabrera@users.noreply.github.com>
2021-12-03 15:42:25 -0800

    Added to control

--- Control/Control.v
+++ Control/Control.v
@@ -1,4 +1,34 @@
 /*INCOMPLETE*/
-module Control(input [5:0] instruction, output RegDst, Branch, MemRead, MemtoReg,ALUOp, MemWrite, ALUSrc, RegWrite);
+module Control(input [5:0] funct, output reg RegDst, Branch, MemRead, MemtoReg,ALUOp, MemWrite, ALUSrc, RegWrite);
 	
+
+always@(funct) begin
+
+	//reseting signals
+	RegDst 	 = 1'b0;
+	Branch 	 = 1'b0;
+	MemRead  = 1'b0;
+	MemtoReg = 1'b0;
+	ALUOp 	 = 1'b0;
+	MemWrite = 1'b0;
+	ALUSrc 	 = 1'b0;
+	RegWrite = 1'b0;
+
+	//Type-R: and, or, nor, add, sub, slt, div, mult, mfhi, mflo
+	if() begin
+
+	end
+
+	//Type-I: addi, lw, sw, beq, 
+	if() begin
+
+	end
+
+	//Type-J: j
+	if() begin
+
+	end
+
+end
+
 endmodule 
