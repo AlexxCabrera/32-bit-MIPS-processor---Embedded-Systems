66d4a0d2b1f8a6af3a7315862fd2ce58e320657c
Alex C <47911421+AlexxCabrera@users.noreply.github.com>
2021-12-05 13:10:28 -0800

    Update ALUcontrol.v

--- ALUcontrol/ALUcontrol.v
+++ ALUcontrol/ALUcontrol.v
@@ -1,33 +1,60 @@
-module ALUcontrol(
-input clk, [1:0] ALUOp, [5:0] Instruction,
+module ALUcontrol(input /*clk,*/[1:0] ALUOp, [5:0] funct,
 output reg [3:0] ALUInput);
 
-  always@(posedge clk)begin //dont know if clk is needed or if we can do @ (ALUOp || Instrrcution)
+always@(/*posedge clk*/ ALUOp, funct)begin
 
-	if(ALUOp == 2'b00) begin
+	if(ALUOp == 2'b00) begin //load or store
 			ALUInput = 4'b0010;
 	end
 
-	else if(ALUOp == 2'bx1)begin
+	else if(ALUOp == 2'b01)begin //Branch eq
+			ALUInput = 4'b0110;
+	end
+
+	else if(ALUOp == 2'b11) begin //add immediate
 			ALUInput = 4'b0010;
 	end
 
-	else if(ALUOp == 2'b1x) begin
-
-		if(Instruction == 6'bxx0000)begin
-			ALUInput = 4'b0010; 
+	else if(ALUOp == 2'b11)begin //Type-R: and, or, nor, add, sub, slt, div, mult, mfhi, mflo
+		//and instruction
+		if(funct == 6'b100100)begin 
+			ALUInput = 4'b0000;
 		end
-
-		else if(Instruction == 6'bxx0010)begin
+		//or instruction
+		if(funct == 6'b100101)begin 
+			ALUInput = 4'b0001;
+		end
+		//nor instruction
+		if(funct == 6'b100111)begin 
+			ALUInput = 4'b1100;
+		end
+		//add instruction
+		if(funct == 6'b100000)begin 
+			ALUInput = 4'b0010;
+		end
+		//sub instruction
+		if(funct == 6'b100010)begin 
 			ALUInput = 4'b0110;
 		end
-
-		else if (Instruction == 6'bxx0000)begin
-			ALUInput = 4'b0001;	
-		end
-
-		else if(Instruction == 6'bxx0010)begin
+		//slt instruction
+		if(funct == 6'b101010)begin 
 			ALUInput = 4'b0111;
+		end
+		//div instruction
+		if(funct == 6'b011010)begin 
+			ALUInput = 4'b0010;
+		end
+		//mult instruction
+		if(funct == 6'b011000)begin 
+			ALUInput = 4'b0110;
+		end
+		//mfhi instruction
+		if(funct == 6'b010000)begin 
+			ALUInput = 4'b0111;
+		end
+		//mflo instruction
+		if(funct == 6'b010010)begin 
+			ALUInput = 4'b1100;
 		end
 	end
 end
