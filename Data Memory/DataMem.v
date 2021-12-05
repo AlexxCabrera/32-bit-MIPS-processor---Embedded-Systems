466799e3e8b1d9a92c750cd22bbef0ec7b3291e3
Alex C <47911421+AlexxCabrera@users.noreply.github.com>
2021-12-05 14:19:41 -0800

    Update DataMem.v

--- Data Memory/DataMem.v
+++ Data Memory/DataMem.v
@@ -1,6 +1,6 @@
 /*DataMem contains 256 memory slots with each slot holding 32 bits on data*/
 
-module DataMem(input MemWrite, MemRead,[7:0] Address, [31:0] Write_data,
+module DataMem(input MemWrite, MemRead,[31:0] Address, [31:0] Write_data,
 output reg [31:0] Read_data );
 
 reg [31:0] memory [255:0]; // 2^32 slots are available in memory but for this project we are using 256 slots
