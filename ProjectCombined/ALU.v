b682a0605934870889bf8c94fb7030e2c697bac3
jebbyx <93552380+jebbyx@users.noreply.github.com>
2021-12-05 02:20:49 -0800

    Update ALU.v

--- ProjectCombined/ALU.v
+++ ProjectCombined/ALU.v
@@ -1,5 +1,5 @@
 `timescale 1ns/1ns
-
+/*
 module ALU( input [31:0] a, 
             input [31:0] b,
             input [3:0] funct,
@@ -23,4 +23,46 @@ always @ (*) begin
     endcase
          zero = (out == 8'b0);
      end
+endmodule */
+
+module ALU(input [31:0] Read_data1, Read_data2, [3:0] ALU_Control,
+output	reg [31:0] ALUresult);
+
+wire [31:0] tempAND, tempOR, tempADD, tempSUB, tempSLT, tempNOR;
+
+
+and_32 and0(.Y(tempAND),.A(Read_data1),.B(Read_data2));
+Or_32 or0(.Y(tempOR),.A(Read_data1),.B(Read_data2));
+thirtytwoBitAdder add0( .A(Read_data1), .B(Read_data2), .Cin(1'b0), .Sub(1'b0), .Sum(tempADD), .Cout() );
+thirtytwoBitAdder sub0( .A(Read_data1), .B(Read_data2), .Cin(1'b0), .Sub(1'b1), .Sum(tempSUB), .Cout() );
+Slt slt0(.Y(tempSLT), .A(Read_data1), .B(Read_data2));
+Nor_32 nor0(.Y(tempNOR),.A(Read_data1),.B(Read_data2));
+
+always@(ALU_Control)begin
+
+	if(ALU_Control == 0000)begin //AND
+		ALUresult = tempAND;
+	end
+
+	else if(ALU_Control == 0001)begin //OR
+		ALUresult = tempOR;
+	end
+
+	else if(ALU_Control == 0010)begin //add
+		ALUresult =  tempADD;
+	end
+
+	else if(ALU_Control == 0110)begin //subtract
+		ALUresult =  tempSUB;
+	end
+
+	else if(ALU_Control == 0111)begin //set-on-less-than
+		ALUresult =  tempSLT;
+	end
+
+	else if(ALU_Control == 1100)begin //NOR
+		ALUresult =  tempNOR;
+	end
+end
+
 endmodule
