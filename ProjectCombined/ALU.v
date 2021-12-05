49cc0c3a5c828fba13f2631dc2c8fc589769affa
Alex C <47911421+AlexxCabrera@users.noreply.github.com>
2021-12-05 13:10:14 -0800

    Update ALU.v

--- ProjectCombined/ALU.v
+++ ProjectCombined/ALU.v
@@ -1,34 +1,7 @@
-`timescale 1ns/1ns
-/*
-module ALU( input [31:0] a, 
-            input [31:0] b,
-            input [3:0] funct,
-            input [4:0] shamt,
-            output reg [31:0] out, 
-            output reg zero);
-
-always @ (*) begin
-
-    case (funct)
-        4'b0000: out = a + b;                             // ADD
-        4'b0001: out = a - b;                             // SUB
-        4'b0010: out = a & b;                             // AND
-        4'b0011: out = a | b;                             // OR
-        4'b0100: out = a ^ b;                             // XOR
-        4'b0101: out = b << shamt;                        // SLL
-        4'b0110: out = b >> shamt;                        // SRL
-        4'b1000: out = $signed(a) < $signed(b) ? 1 : 0;   // SLT
-        4'b1010: out = ~ (a | b);                         // NOR 
-
-    endcase
-         zero = (out == 8'b0);
-     end
-endmodule */
-
 module ALU(input [31:0] Read_data1, Read_data2, [3:0] ALU_Control,
 output	reg [31:0] ALUresult);
 
-wire [31:0] tempAND, tempOR, tempADD, tempSUB, tempSLT, tempNOR;
+wire [31:0] tempAND, tempOR, tempADD, tempSUB, tempSLT, tempNOR, tempDiv, tempRemainder, tempMult;
 
 
 and_32 and0(.Y(tempAND),.A(Read_data1),.B(Read_data2));
@@ -37,6 +10,9 @@ thirtytwoBitAdder add0( .A(Read_data1), .B(Read_data2), .Cin(1'b0), .Sub(1'b0), 
 thirtytwoBitAdder sub0( .A(Read_data1), .B(Read_data2), .Cin(1'b0), .Sub(1'b1), .Sum(tempSUB), .Cout() );
 Slt slt0(.Y(tempSLT), .A(Read_data1), .B(Read_data2));
 Nor_32 nor0(.Y(tempNOR),.A(Read_data1),.B(Read_data2));
+Divider div0(.A(Read_data1), .B(Read_data2), .result(tempDiv), .remainder(tempRemainder));
+multiplier_16x16 mult0(.product(tempMult),.A(Read_data1),.B(Read_data2));
+
 
 always@(ALU_Control)begin
 
@@ -62,6 +38,22 @@ always@(ALU_Control)begin
 
 	else if(ALU_Control == 1100)begin //NOR
 		ALUresult =  tempNOR;
+	end
+
+	else if(ALU_Control == 0010)begin //div
+		ALUresult =  tempDiv;
+	end
+
+	else if(ALU_Control == 0110)begin //mult
+		ALUresult =  tempMult;
+	end
+
+	else if(ALU_Control == 0111)begin //mfhi
+		ALUresult =  tempMfhi;
+	end
+
+	else if(ALU_Control == 1100)begin //mflo
+		ALUresult =  tempMflo;
 	end
 end
 
